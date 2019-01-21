7341037f877ed67de6933e3829995778cd4d8e8b
Joseph Robinson <smartech56@gmail.com>
2019-01-21 09:19:39 -0700

    added help command to startxcli

--- startxcli.sh
+++ startxcli.sh
@@ -1,1 +1,11 @@
 # !/bin/bash
+read -p "StartX> " -r
+if [[ $REPLY = help ]]
+then
+    echo "Commands:"
+    echo "i3 - starts I3"
+    echo "lxde - starts LXDE"
+    echo "xfce - starts XFCE4"
+    echo "gnome - starts the gnome shell"
+    echo "openbox - starts OpenBox"
+fi
