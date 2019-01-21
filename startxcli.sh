ef3e30754cc62326043158478cd19e68cf376846
Joseph Robinson <smartech56@gmail.com>
2019-01-21 10:04:25 -0700

    added comments and check for sudo

--- startxcli.sh
+++ startxcli.sh
@@ -1,10 +1,21 @@
 # !/bin/bash
+
+# Check for sudo
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Warning about .xinitrc being moved
 echo "Warning: your current .xinitrc config will be renamed to xinit"
 
 read -p "Continue? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+    # Initialize if user agreed to .xinitrc being moved
+    cd
+    sudo mv .xinitrc xinit
     clear
     read -p "StartX> " -r
 fi
@@ -18,4 +29,5 @@ then
     echo "xfce - starts XFCE4"
     echo "gnome - starts the gnome shell"
     echo "openbox - starts OpenBox"
+    echo "htop - opens htop"
 fi
