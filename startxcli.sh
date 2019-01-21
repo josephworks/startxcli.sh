71b02a5035d7c0f3e0ab6ed7121f92b9b67d2f0a
Joseph Robinson <smartech56@gmail.com>
2019-01-21 09:28:19 -0700

    added a warning message on startup

--- startxcli.sh
+++ startxcli.sh
@@ -1,8 +1,17 @@
 # !/bin/bash
-read -p "StartX> " -r
+echo "Warning: your current .xinitrc config will be renamed to xinit"
+
+read -p "Continue? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    read -p "StartX> " -r
+fi
+
 if [[ $REPLY = help ]]
 then
     echo "Commands:"
+    echo "revert - reverts your .xinitrc file"
     echo "i3 - starts I3"
     echo "lxde - starts LXDE"
     echo "xfce - starts XFCE4"
